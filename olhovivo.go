804de97bec9c16b0f8437928794ab65bdc856848
Nikolas Silva <nikolas.rsilva@gmail.com>
2018-03-14 19:45:19 -0300

    Add QueryLines method

--- olhovivo.go
+++ olhovivo.go
@@ -1,11 +1,13 @@
 package olhovivo
 
 import (
-	"fmt"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strconv"
+
+	"github.com/pkg/errors"
 )
 
 const (
@@ -17,7 +19,18 @@ type OlhoVivo struct {
 	URL     string
 	Version string
 	Token   string
-	client  *http.Client
+
+	client *http.Client
+}
+
+type BusLine struct {
+	Cl int    `json:"cl"`
+	Lc bool   `json:"lc"`
+	Lt string `json:"lt"`
+	Tl int    `json:"tl"`
+	Sl int    `json:"sl"`
+	Tp string `json:"tp"`
+	Ts string `json:"ts"`
 }
 
 func New(token string) *OlhoVivo {
@@ -34,16 +47,53 @@ func (ov *OlhoVivo) Authenticate() (ok bool, err error) {
 	})
 
 	if err != nil {
-		return
+		return false, errors.Wrap(err, "request error")
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return
+		return false, errors.Wrap(err, "error while reading response body")
 	}
 
 	defer resp.Body.Close()
-	return strconv.ParseBool(string(body))
+
+	ok, err = strconv.ParseBool(string(body))
+	if err != nil {
+		return false, errors.Wrap(err, "error while parsing response body")
+	}
+
+	return
+}
+
+func (ov *OlhoVivo) QueryLines(search string) (lines []BusLine, err error) {
+	// TODO: move authentication to another place
+	ok, err := ov.Authenticate()
+	if err != nil {
+		return nil, errors.Wrap(err, "error on OlhoVivo authentication")
+	}
+	if !ok {
+		return nil, errors.New("failed to authenticate")
+	}
+
+	resp, err := ov.request("GET", "/Linha/Buscar", url.Values{
+		"termosBusca": []string{search},
+	})
+
+	if err != nil {
+		return nil, errors.Wrap(err, "request error")
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Wrap(err, "error while reading response body")
+	}
+
+	defer resp.Body.Close()
+	if err := json.Unmarshal(body, &lines); err != nil {
+		return nil, errors.Wrap(err, "error while unmarshaling response body")
+	}
+
+	return
 }
 
 func (ov *OlhoVivo) request(method, path string, params url.Values) (resp *http.Response, err error) {
@@ -51,21 +101,27 @@ func (ov *OlhoVivo) request(method, path string, params url.Values) (resp *http.
 		ov.client = &http.Client{}
 	}
 
-	parsedUrl, err := ov.mountUrl(path)
+	parsedUrl, err := ov.mountUrl(path, params)
 	if err != nil {
-		return
+		return nil, errors.Wrap(err, "error while mounting request url")
 	}
 
 	req := &http.Request{
-		Method:   method,
-		URL:      parsedUrl,
-		PostForm: params,
+		Method: method,
+		URL:    parsedUrl,
 	}
 
 	return ov.client.Do(req)
 }
 
-func (ov *OlhoVivo) mountUrl(path string) (parsedUrl *url.URL, err error) {
-	urlStr := fmt.Sprintf("%s/%s/%s", ov.URL, ov.Version, path)
-	return url.Parse(urlStr)
+func (ov *OlhoVivo) mountUrl(path string, params url.Values) (parsedUrl *url.URL, err error) {
+	parsedUrl, err = url.Parse(ov.URL)
+	if err != nil {
+		return nil, errors.Wrap(err, "error while parsing OlhoVivo url")
+	}
+
+	parsedUrl.Path = "/" + ov.Version + path
+	parsedUrl.RawQuery = params.Encode()
+
+	return
 }
--- olhovivo_test.go
+++ olhovivo_test.go
@@ -3,20 +3,23 @@ package olhovivo
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
-func TestAuthenticate(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("true"))
-	}))
+const (
+	API_TEST_VERSION = "v2.1"
+	API_TEST_TOKEN   = "l00k4tm3l00k4tm3th3m0nst3r1ns1d30fm3h4sgr0wnth1sl4rg3"
+)
 
+func TestAuthenticate(t *testing.T) {
+	ts := createTestServer(t)
 	defer ts.Close()
 
 	ov := &OlhoVivo{
 		URL:     ts.URL,
-		Version: "v2.1",
-		Token:   "l00k4tm3l00k4tm3th3m0nst3r1ns1d30fm3h4sgr0wnth1sl4rg3",
+		Version: API_TEST_VERSION,
+		Token:   API_TEST_TOKEN,
 	}
 
 	ok, err := ov.Authenticate()
@@ -27,4 +30,75 @@ func TestAuthenticate(t *testing.T) {
 	if !ok {
 		t.Errorf("expected authentication to be '%v', but got '%v'", true, ok)
 	}
+}
+
+func TestQueryLine(t *testing.T) {
+	ts := createTestServer(t)
+	defer ts.Close()
+
+	ov := &OlhoVivo{
+		URL:     ts.URL,
+		Version: API_TEST_VERSION,
+		Token:   API_TEST_TOKEN,
+	}
+
+	_, err := ov.QueryLines("8000")
+	if err != nil {
+		t.Errorf("error while querying lines: %s", err.Error())
+	}
+}
+
+func createTestServer(t *testing.T) *httptest.Server {
+	t.Helper()
+
+	var authenticated bool
+
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		path := strings.TrimPrefix(r.URL.Path, "/"+API_TEST_VERSION)
+
+		switch path {
+		case "/Login/Autenticar":
+			if r.FormValue("token") == "" {
+				t.Errorf("expected 'token' form value to be set")
+			}
+
+			authenticated = true
+			w.Write([]byte("true"))
+		case "/Linha/Buscar":
+			if !authenticated {
+				t.Errorf("http client is not authenticated")
+			}
+
+			if r.FormValue("termosBusca") == "" {
+				t.Errorf("expected 'termosBusca' form value to be set")
+			}
+
+			jsonString := `
+				[
+					{
+						"cl": 1273,
+						"lc": false,
+						"lt": "8000",
+						"sl": 1,
+						"tl": 10,
+						"tp": "PCA.RAMOS DE AZEVEDO",
+						"ts": "TERMINAL LAPA"
+					},
+					{
+						"cl": 34041,
+						"lc": false,
+						"lt": "8000",
+						"sl": 2,
+						"tl": 10,
+						"tp": "PCA.RAMOS DE AZEVEDO",
+						"ts": "TERMINAL LAPA"
+					}
+				]
+			`
+
+			w.Write([]byte(jsonString))
+		default:
+			w.WriteHeader(http.StatusNotFound)
+		}
+	}))
 }
